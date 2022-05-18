e518a4c9f96d42a5635112c8a06ae03dd2fb3c9d
Poyu Juan <85391113+Poyuiu@users.noreply.github.com>
2022-05-19 02:18:02 +0800

    SMTX_print mistake 5
    required output format '<>'

--- SMatrixADT.c
+++ SMatrixADT.c
@@ -151,6 +151,7 @@ int SMTX_print(SMatrixType A[])
     {
         for(i=1; i<=v; i++)
         {
+            // TODO
             printf("%2d, %2d, %2d\n",A->R[i].row,A->R[i].col, A->R[i].value);
         // TODO
     }
